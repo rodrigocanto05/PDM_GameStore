29d760d316c58e8ca1c0e496cf19676fb930f757
Rodrigo Canto <email>
2025-11-19 10:56:19 +0000

    comit test

--- app/src/main/java/pt/iade/ei/gamestore/MainActivity.kt
+++ app/src/main/java/pt/iade/ei/gamestore/MainActivity.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pt.iade.ei.gamestore.ui.theme.GameStoreTheme
 
-class MainActivity : ComponentActivity() { // teste comit
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : ComponentActivity() { // teste comit agora
+    override fun onCreate(savedInstanceState: Bundle?) { //2 teste
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
